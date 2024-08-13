a8ad75e6cada98943ed731d26a7d670df175c375
Ozodbek <ozodbekhimmatov@gmail.com>
2024-08-13 15:39:14 +0500

    responsive first page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,24 +1,33 @@
-import "./home.css";
 import Navbar from "../navbar/navbar";
 import illustration from "./Illustration.png";
 import { KeyboardArrowDown } from "@mui/icons-material";
 
 const home = () => {
   return (
-    <div className="home">
+    <div className="home xl:h-[115vh] h-screen w-[93%] sm:w-[80%] lg:w-[85%] xl:w-[1109px] m-auto">
       <Navbar />
-      <div className="home-banner-container">
-        <div className="home-text-section">
-          <h1>Track your Expenses & Save Money</h1>
-          <h6>Helps you to organize your income and expenses</h6>
-          <div className="free-demo">
-            <button className="rounded-lg">
-              Try free demo <KeyboardArrowDown fontSize="large" />
+      <div className="home-banner-container flex flex-col sm:flex-row justify-between xl:mt-12 md:mt-5 sm:mt-8 mt-2 items-center gap-10 m-auto">
+        <div className="home-text-section lg:mt-2 md:mt-1 lg:w-[384px] md:w-[378px] sm:w-[230px] w-[px] xl:w-auto sm:text-left text-center">
+          <h1 className="xl:text-8xl lg:text-7xl md:text-6xl sm:text-4xl text-6xl xl:w-auto text-[#292830] font-bold">
+            Track your Expenses & Save Money
+          </h1>
+          <h6 className="xl:text-2xl lg:text-lg md:text-sm sm:text-xs text-lg text-[#bdbdbd] xl:mt-5 xl:mb-10 sm:mt-3 mt-4 sm:mb-5 mb-6 hover:text-[#292830] cursor-context-menu">
+            Helps you to organize your income and expenses
+          </h6>
+          <div className="free-demo flex sm:justify-normal justify-center items-center m-auto xl:gap-6 sm:gap-3 text-center">
+            <button className="flex items-center justify-center bg-[#ff7235] text-white xl:text-2xl lg:text-lg md:text-sm sm:text-xs xl:px-6 xl:py-5 lg:px-4 lg:py-3 md:p-2 sm:p-1 p-1 md:rounded-lg rounded-md">
+              <span>Try free demo</span> <KeyboardArrowDown />
             </button>
-            <p>— Web, iOS and Android</p>
+            <p className="flex items-center text-[#bdbdbd] xl:text-2xl lg:text-lg md:text-sm sm:text-xs">
+              <span>—</span> <span>Web, iOS and Android</span>
+            </p>
           </div>
         </div>
-        <img src={illustration} alt="" />
+        <img
+          src={illustration}
+          className="xl:h-auto lg:h-96 md:h-56 sm:h-56 sm:m-0 mb-5"
+          alt=""
+        />
       </div>
     </div>
   );
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,114 @@
-import "./navbar.css";
+import { useState } from "react";
 import Logo from "./Logo.svg";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import { Category, Info, Phone, Sell, Feedback } from "@mui/icons-material";
 
 const navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Features",
+      icon: <Category />
+    },
+    {
+      text: "About us",
+      icon: <Info />
+    },
+    {
+      text: "Pricing",
+      icon: <Sell />
+    },
+    {
+      text: "Feedback",
+      icon: <Feedback />
+    },
+    {
+      text: "Request a demo",
+      icon: <Phone />
+    }
+  ];
+
   return (
-    <nav className="">
-      <div className="nav-logo-container">
-        <a href="#">
-          <img src={Logo} alt="Xpense" />
+    <nav className="flex items-center	justify-between lg:min-h-40 sm:min-h-24 min-h-20">
+      <a href="#">
+        <img src={Logo} alt="Xpense" />
+      </a>
+      <div className="hidden md:flex">
+        <a
+          href=""
+          className="xl:mr-12 lg:mr-8 md:mr-4 text-[#292830] lg:text-lg md:text-sm"
+        >
+          Features
+        </a>
+        <a
+          href=""
+          className="xl:mr-12 lg:mr-8 md:mr-4 text-[#292830] lg:text-lg md:text-sm"
+        >
+          About us
+        </a>
+        <a
+          href=""
+          className="xl:mr-12 lg:mr-8 md:mr-4 text-[#292830] lg:text-lg md:text-sm"
+        >
+          Pricing
+        </a>
+        <a
+          href=""
+          className="xl:mr-12 lg:mr-8 md:mr-4 text-[#292830] lg:text-lg md:text-sm"
+        >
+          Feedback
         </a>
       </div>
-      <div className="navbar-links-container">
-        <a href="">Features</a>
-        <a href="">About us</a>
-        <a href="">Pricing</a>
-        <a href="">Feedback</a>
+      <button className="hidden md:flex border border-[#ff7235] text-[#ff7235] rounded-lg lg:text-lg md:text-sm px-3.5 py-2 hover:bg-[#ff7235] hover:text-white">
+        Request a demo
+      </button>
+      <div className="navbar-menu-container  md:hidden">
+        <HiOutlineBars3
+          onClick={() => setOpenMenu(true)}
+          className="text-2xl"
+        />
       </div>
-      <button className="request">Request a demo</button>
+      <Drawer
+        open={openMenu}
+        onClose={() => setOpenMenu(false)}
+        anchor="right"
+        className="opacity-90"
+      >
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem
+                key={item.text}
+                disablePadding
+                className="hover:bg-slate-500"
+              >
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText
+                    primary={item.text}
+                    className="text-[#292830]"
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
     </nav>
   );
 };
